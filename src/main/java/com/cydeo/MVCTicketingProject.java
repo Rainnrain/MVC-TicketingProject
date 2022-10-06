5b3cb93a2f4c02d4ebd4e16e50c71509f1d2dc64
Lorraine <lorrainecannom@gmail.com>
2022-10-05 22:54:55 -0400

    Added validation

--- src/main/java/com/cydeo/MVCTicketingProject.java
+++ src/main/java/com/cydeo/MVCTicketingProject.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TicketingProjectMvcApplication {
+public class MVCTicketingProject {
 
     public static void main(String[] args) {
-        SpringApplication.run(TicketingProjectMvcApplication.class, args);
+        SpringApplication.run(MVCTicketingProject.class, args);
     }
 
 }
